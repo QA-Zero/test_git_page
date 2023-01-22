da00e48edba9e655aa3076d729d42aaae25b80bf
Ivan <solvegen.test@gmail.com>
2023-01-22 15:46:57 +0200

    Added project stub

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <main-page msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+
+<script>
+
+import MainPage from '@/components/MainPage.vue'
+
+export default {
+  name: 'App',
+  components: { MainPage },
+}
+</script>
+
+
+<style>
+
+body {
+  height: 100vh;
+  width: 100vw;
+  margin: 0;
+}
+
+#app {
+  height: 100vh;
+  width: 100vw;
+  color: #2c3e50;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="main-page">Hello, world !!!</div>
+</template>
+
+
+<script>
+
+export default {
+  name: 'MainPage',
+  data() {
+    return {
+    }
+  },
+}
+</script>
+
+
+<style lang="scss">
+
+.main-page {
+  height: 100vh;
+  width: 100vw;
+  font-size: 70px;
+  color: #00008b;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
